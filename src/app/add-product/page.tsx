62d8931b555bed81f42d0c507bc1331a58b32bc5
Digvijay singh <sdigvijay269@gmail.com>
2025-07-16 13:04:07 +0530

    fixed button and loading error

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,17 +1,40 @@
-"use client";
-
+import prisma from '@/lib/prisma';
 import React from 'react';
 import Button from '@/components/button/page';
+import { redirect } from 'next/navigation';
 
+async function addProduct(formData: FormData) {
+    'use server';
+
+    const name = formData.get('name') as string;
+    const description = formData.get('description') as string;
+    const imageUrl = formData.get('imageUrl') as string;
+    const price = Number(formData.get('price') || 0);
+
+    // Here you would typically call your database or API to add the product
+    // For example:
+    // await prisma.product.create({
+    //     data: { name, description, imageUrl, price }
+    // });
+
+    if(!name || !description || !imageUrl || isNaN(price) || price <= 0) {
+        throw new Error('missing or invalid product data');
+    }
+    
+    await prisma.product.create({
+        data: {name, description, imageUrl, price},
+    });
+
+ redirect("/"); // Redirect to the home page after adding the product
+}
 export default function AddProductPage() {
-
     return (
         <div>
             <> 
             <h1 className="mb-3 text-lg font-bold">Add Product</h1>
             </>
 
-            <form>
+            <form action={addProduct}>
                 <input
                  required
                  name="name"
@@ -38,7 +61,7 @@ export default function AddProductPage() {
                  type="number"
                  className="input-bordered input mb-3 w-full"/>
                  
-                 <Button className ="w-full">Add Product</Button>
+                 <Button className ="w-full" >Add Product</Button>
 
 
             </form>
--- src/components/button/page.tsx
+++ src/components/button/page.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+"use client";
 
-export default function Button({ children, className }: { children: React.ReactNode; className?: string }) {
-  return (
-    <button className={`btn btn-outline btn-accent ${className || ''}`}>
+import React, { ComponentProps } from "react";
+import { useFormStatus, } from "react-dom";
+
+export default function Button(
+  { children,
+    className,
+    ...props
+  }: 
+  { children: React.ReactNode; className?: string } & ComponentProps<'button'>  ) 
+  {
+  const { pending } = useFormStatus();
+    return (
+    <button 
+    {...props}
+    className={`btn btn-outline btn-accent ${className}`}
+    type='submit'
+    disabled={pending || props.disabled}
+    >
+      {pending && <span className="loading loading-spinner loading-sm"></span>}
       {children}
     </button>
   );
