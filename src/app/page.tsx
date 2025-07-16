5d655157d074807b2a84618920b9eb5ff7de9b4c
Digvijay singh <sdigvijay269@gmail.com>
2025-07-16 21:16:32 +0530

    added card component on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
-"use client"
-
 import React from 'react';
+import ProductCard from '@/components/card/page';
+import prisma from '@/lib/prisma';
 
-export default function home() {
+export default async function home() {
+
+
+    const product = await prisma.product.findMany({
+        orderBy: {id:"desc"},
+    })
+
     return (
         <div>
             
             <>
                 <h1 className="text-center font-bold text-5xl">WELCOME TO THE STORE</h1>
                 {/* <Navbar className= "w-full" /> */}
+
+               <ProductCard product={product[0]}  />
             </>
         </div>
     )
--- src/components/card/page.tsx
+++ src/components/card/page.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
+import Link  from 'next/link';
+import type { Product } from '../../generated/prisma';
+import PriceTag from '../priceTag/page';
 import Image from 'next/image';
 
-const ProductCard: React.FC = () => {
-  return (
-    <div className="card bg-base-100 w-96 shadow-sm">
-      <figure>
-        <Image
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default ProductCard;
+interface ProductCardProps {
+  product: Product;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+
+  const isNew =  Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 *24 * 7;
+
+  return (
+    <>
+    <Link href={`/product/${product.id}`} className="card w-auto m-auto bg-blue-200 text-black hover:shadow-x2 transition-opacity">
+      <figure>
+        {product.imageUrl ? (
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+            width={400}
+            height={300}
+            className='h-auto object-cover'
+          />) : null}
+      </figure>
+
+      <div className= 'card-body'>
+        <h2 className="card-title">
+          {product.name}
+          </h2>
+          {isNew && <div className='badge badge-secondary'>New</div>}
+          <p>
+            {product.description}
+          </p>
+          <PriceTag price={product.price} />
+      </div>
+      
+    </Link>
+    </>
+  )
+}
