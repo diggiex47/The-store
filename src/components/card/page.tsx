c715a8eeb031a2176afb8a24bb234ae5565d668a
Digvijay singh <sdigvijay269@gmail.com>
2025-07-21 19:31:03 +0530

    dazzy added cart funtion and it's components

--- src/components/card/page.tsx
+++ src/components/card/page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import Link  from 'next/link';
-import type { Product } from '../../generated/prisma';
-import PriceTag from '../priceTag/page';
-import Image from 'next/image';
-
+import React from "react";
+import Link from "next/link";
+import type { Product } from "../../generated/prisma";
+import PriceTag from "../priceTag/page";
+import Image from "next/image";
 
 interface ProductCardProps {
   product: Product;
@@ -11,35 +10,36 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
-
-  const isNew =  Date.now() - new Date(product.createdAt).getTime() < 1000 * 60 * 60 *24 * 7;
+  const isNew =
+    Date.now() - new Date(product.createdAt).getTime() <
+    1000 * 60 * 60 * 24 * 7;
 
   return (
-    <div className="p-2 ">
-    <Link href={`/product/${product.id}`} className="card bg-green-100 shadow-xl text-black hover:shadow-x1 hover:shadow-cyan-300">
-      <figure className=" h-64 w-full overflow-hidden">
-        {product.imageUrl ? (
-          <Image
-            src={product.imageUrl}
-            alt={product.name}
-            width={400}
-            height={400}
-            className='h-auto object-cover'
-          />) : null}
-      </figure>
+    <div>
+      <Link
+        href={"/Product/" + product.id}
+        className="card hover:shadow-x1 bg-green-100 text-black shadow-xl hover:shadow-cyan-300"
+      >
+        <figure className="h-64 w-full overflow-hidden">
+          {product.imageUrl ? (
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              width={400}
+              height={400}
+              className="h-auto object-cover"
+            />
+          ) : null}
+        </figure>
 
-      <div className= 'card-body p-4'>
-        <h2 className="card-title">
-          {product.name}
-          </h2>
-          {isNew && <div className='badge badge-secondary'>New</div>}
-          <p>
-            {product.description}
-          </p>
+        <div className="card-body p-4">
+          <h2 className="card-title">{product.name}</h2>
+          {isNew && <div className="badge badge-secondary">New</div>}
+          <p>{product.description}</p>
           <PriceTag price={product.price} />
-      </div>
-      
-    </Link>
+          <button className="btn btn-primary mt-2">Buy now</button>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
