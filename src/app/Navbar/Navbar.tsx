4f5cd547c7fefde4a0aee87d34f9dfb512c8ff20
Adii9669 <adityrajsingh799@gmail.com>
2025-07-22 19:39:29 +0530

    signIn auth completed

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 // src/app/Navbar/Navbar.tsx
 
-
 import Link from "next/link";
 import ThemeToggle from "./ThemeToggle"; // <-- IMPORT
 import { searchProducts } from "@/components/SearchProduct/page"; // Import the search function
@@ -8,31 +7,30 @@ import UserMenuBtn from "./UserMenuBtn";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
-
-
 export default async function Navbar() {
-
   const session = await getServerSession(authOptions);
   return (
-    <div className="bg-base-100 fixed top-0 left-0 right-0 z-50 h-16 backdrop-blur-sm">
+    <div className="bg-base-100 fixed top-0 right-0 left-0 z-50 h-16 backdrop-blur-sm">
       <div className="navbar m-auto flex h-full max-w-7xl flex-col sm:flex-row">
         <div className="flex-1">
-          <Link href="/" className="btn btn-ghost text-xl normal-case">The Store</Link>
+          <Link href="/" className="btn btn-ghost text-xl normal-case">
+            The Store
+          </Link>
         </div>
         <div className="flex-none gap-2">
           <form action={searchProducts}>
             <div className="form-control">
-                <input 
+              <input
                 name="searchQuery"
                 placeholder="Search"
-                className="input input-bordered w-full min-w-[150px] max-w-xs"
-                ></input> </div>
+                className="input input-bordered w-full max-w-xs min-w-[150px]"
+              ></input>{" "}
+            </div>
           </form>
           {/* PLACE THE TOGGLE HERE */}
-         
         </div>
-         <ThemeToggle /> 
-         <UserMenuBtn session={session} /> 
+        <ThemeToggle />
+        <UserMenuBtn session={session} />
       </div>
     </div>
   );
--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,12 +1,12 @@
-import zod from "zod";
+import zod  from "zod";
 
 const envSchema = zod.object({
   DATABASE_URL: zod.string().nonempty(),
   Google_Client_id: zod.string().nonempty(),
   Google_secret: zod.string().nonempty(),
-  NEAUTH_SECRET: zod.string().nonempty(),
-  NEXTAUTH_URL: zod.string().nonempty(),
-  NEAUTH_CLIENT_ID: zod.string().nonempty(),
+  NEXAUTH_SECRET: zod.string().nonempty(),
+  NEXTAUTH_URL: zod.string().url(),
+  NEXAUTH_CLIENT_ID: zod.string().nonempty(),
 });
 
 export const env = envSchema.parse(process.env);
